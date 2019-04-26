1f5bff711d5e3f0a307aa1049d3d68ab9157312e
Eva Deželak <eva.dezelak@gmail.com>
2019-04-26 11:58:07 +0200

    priklop na bazo

---
+++ uvoz.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+# uvozimo bottle.py
+from bottle import *
+
+# uvozimo ustrezne podatke za povezavo
+import auth as auth
+
+# uvozimo psycopg2
+import psycopg2, psycopg2.extensions, psycopg2.extras
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
+
+
+
+# Glavni program
+
+# priklopimo se na bazo
+conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password)
+conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemogočimo transakcije
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
+
+# poženemo strežnik na portu 8080, glej http://localhost:8000/
+run(host='localhost', port=8000)
