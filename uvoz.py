437177035b8b2d38f34823cbd232e341a96bd393
Katarina Brilej <katarina.brilej@gmail.com>
2019-05-20 14:07:29 +0200

    popravljena baza

--- uvoz.py
+++ uvoz.py
@@ -20,51 +20,78 @@ conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, passwo
 conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemogočimo transakcije
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
 
-cur.execute("DROP TABLE IF EXISTS Podrocje CASCADE")
-cur.execute("""CREATE TABLE Podrocje(Id SERIAL PRIMARY KEY, Celina TEXT, 
-Drzava TEXT, Mesto TEXT, Okrozje TEXT)""")
-cur.execute("INSERT INTO Podrocje (Celina, Drzava, Mesto, Okrozje) VALUES ('Severna Amerika', 'ZDA', 'Los Angeles', 'neki')")
+cur.execute("DROP TABLE IF EXISTS Celina CASCADE")
+cur.execute("CREATE TABLE Celina(Id SERIAL PRIMARY KEY, Ime_celine TEXT)")
+cur.execute("INSERT INTO Celina (Ime_celine) VALUES ('Severna Amerika')")
+cur.execute("INSERT INTO Ugodnosti (Ime_ugodnosti) VALUES ('Južna Amerika')")
+cur.execute("INSERT INTO Ugodnosti (Ime_ugodnosti) VALUES ('Afrika')")
+cur.execute("INSERT INTO Ugodnosti (Ime_ugodnosti) VALUES ('Evropa')")
+cur.execute("INSERT INTO Ugodnosti (Ime_ugodnosti) VALUES ('Avstralija')")
+cur.execute("INSERT INTO Ugodnosti (Ime_ugodnosti) VALUES ('Azija')")
+cur.execute("INSERT INTO Ugodnosti (Ime_ugodnosti) VALUES ('Antarktika')")
+
+cur.execute("DROP TABLE IF EXISTS Drzava CASCADE")
+cur.execute("""CREATE TABLE Drzava(Id SERIAL PRIMARY KEY, Ime_drzave TEXT, Celina_id INT, 
+FOREIGN KEY(Celina_id) REFERENCES Celina(Id))""")
+cur.execute("INSERT INTO Drzava (Ime_drzave) VALUES ('ZDA')")
+cur.execute("INSERT INTO Drzava (Ime_drzave) VALUES ('Kostarika')")
+cur.execute("INSERT INTO Drzava (Ime_drzave) VALUES ('Panama')")
+cur.execute("INSERT INTO Drzava (Ime_drzave) VALUES ('Nemčija')")
+cur.execute("INSERT INTO Drzava (Ime_drzave) VALUES ('Poljska')")
+
+
+cur.execute("DROP TABLE IF EXISTS Mesto CASCADE")
+cur.execute("""CREATE TABLE Mesto(Id SERIAL PRIMARY KEY, Ime_mesta TEXT, Drzava_id INT, 
+FOREIGN KEY(Drzava_id) REFERENCES Drzava(Id))""")
+cur.execute("INSERT INTO Mesto (Ime_mesta) VALUES ('Los Angeles')")
+cur.execute("INSERT INTO Mesto (Ime_mesta) VALUES ('San Jose')")
+cur.execute("INSERT INTO Mesto (Ime_mesta) VALUES ('Panama City')")
+cur.execute("INSERT INTO Mesto (Ime_mesta) VALUES ('Honolulu')")
+cur.execute("INSERT INTO Mesto (Ime_mesta) VALUES ('Berlin')")
+cur.execute("INSERT INTO Mesto (Ime_mesta) VALUES ('Krakow')")
+
+
+cur.execute("DROP TABLE IF EXISTS Lokacija CASCADE")
+cur.execute("""CREATE TABLE Lokacija(Id SERIAL PRIMARY KEY, Ime_lokacije TEXT, Tip TEXT, Mesto_id INT, 
+FOREIGN KEY(Mesto_id) REFERENCES Mesto(Id))""")
+
+cur.execute("DROP TABLE IF EXISTS Hotel CASCADE")
+cur.execute("""CREATE TABLE Hotel(Id SERIAL PRIMARY KEY, Ime TEXT, st_zvezdic INT, 
+tip_nastanitve TEXT, Drzava_id INT, Mesto_id INT,
+FOREIGN KEY (Drzava_id) REFERENCES Drzava(Id) ON UPDATE CASCADE ON DELETE CASCADE,
+FOREIGN KEY (Mesto_id) REFERENCES Mesto(Id) ON UPDATE CASCADE ON DELETE CASCADE)""")
+cur.execute("INSERT INTO Hotel (Ime, st_zvezdic, tip_nastanitve) VALUES ('The Beverly Hills Hotel', 5, 'hotel')")
+
+
+cur.execute("DROP TABLE IF EXISTS Na_lokaciji CASCADE")
+cur.execute("""CREATE TABLE Na_lokaciji(Hotel_id INT , Lokacija_id INT ,
+PRIMARY KEY (Hotel_id, Lokacija_id), FOREIGN KEY (Hotel_id) REFERENCES Hotel(Id) ON UPDATE CASCADE ON DELETE CASCADE, 
+FOREIGN KEY (Lokacija_id) REFERENCES Lokacija(Id) ON UPDATE CASCADE ON DELETE CASCADE)""")
 
 
 cur.execute("DROP TABLE IF EXISTS Ugodnosti CASCADE")
 cur.execute("CREATE TABLE Ugodnosti(Id SERIAL PRIMARY KEY, Ime_ugodnosti TEXT)")
 cur.execute("INSERT INTO Ugodnosti (Ime_ugodnosti) VALUES ('bazen')")
 cur.execute("INSERT INTO Ugodnosti (Ime_ugodnosti) VALUES ('zajtrk')")
-
-
-cur.execute("DROP TABLE IF EXISTS Lokacija CASCADE")
-cur.execute("""CREATE TABLE Lokacija(Id SERIAL PRIMARY KEY, Ime TEXT,
-Podrocje INT REFERENCES Podrocje(Id) ON UPDATE CASCADE ON DELETE CASCADE)""")
-cur.execute("INSERT INTO Lokacija (Ime, Podrocje) VALUES ('The Beverly Hills Hotel', 1)")
-
-
-cur.execute("DROP TABLE IF EXISTS Hotel CASCADE")
-cur.execute("""CREATE TABLE Hotel(Id SERIAL PRIMARY KEY, Ime TEXT, st_zvezdic INT, 
-tip_nastanitve TEXT, kapaciteta INT,
-Lokacija INT REFERENCES Lokacija(Id) ON UPDATE CASCADE ON DELETE CASCADE)""")
-cur.execute("INSERT INTO Hotel (Ime, st_zvezdic, tip_nastanitve, kapaciteta, Lokacija) VALUES ('The Beverly Hills Hotel', 5, 'hotel', 100, 1)")
+cur.execute("INSERT INTO Ugodnosti (Ime_ugodnosti) VALUES ('klima')")
+cur.execute("INSERT INTO Ugodnosti (Ime_ugodnosti) VALUES ('spa center')")
+cur.execute("INSERT INTO Ugodnosti (Ime_ugodnosti) VALUES ('fitnes')")
+cur.execute("INSERT INTO Ugodnosti (Ime_ugodnosti) VALUES ('restavracija')")
+cur.execute("INSERT INTO Ugodnosti (Ime_ugodnosti) VALUES ('parkirišče')")
 
 
 cur.execute("DROP TABLE IF EXISTS Ima CASCADE")
-cur.execute("""CREATE TABLE Ima(Hotel INT REFERENCES Hotel(Id) ON UPDATE CASCADE ON DELETE CASCADE,
-Ugodnost INT REFERENCES Ugodnosti(Id) ON UPDATE CASCADE ON DELETE CASCADE, PRIMARY KEY (Hotel, Ugodnost))""")
+cur.execute("""CREATE TABLE Ima(Hotel INT, Ugodnost INT , PRIMARY KEY (Hotel, Ugodnost),
+FOREIGN KEY (Hotel) REFERENCES Hotel(Id) ON UPDATE CASCADE ON DELETE CASCADE
+, FOREIGN KEY (Ugodnost) REFERENCES Ugodnosti(Id) ON UPDATE CASCADE ON DELETE CASCADE)""")
 cur.execute("INSERT INTO Ima VALUES (1, 1)")
 cur.execute("INSERT INTO Ima VALUES (1, 2)")
 
 
-cur.execute("DROP TABLE IF EXISTS Znacilnosti CASCADE")
-cur.execute("CREATE TABLE Znacilnosti(Id SERIAL PRIMARY KEY, Ime_znacilnosti TEXT)")
-cur.execute("INSERT INTO Znacilnosti (Ime_znacilnosti) VALUES ('smučišče')")
-cur.execute("INSERT INTO Znacilnosti (Ime_znacilnosti) VALUES ('plaža')")
-cur.execute("INSERT INTO Znacilnosti (Ime_znacilnosti) VALUES ('center mesta')")
-cur.execute("INSERT INTO Znacilnosti (Ime_znacilnosti) VALUES ('gore')")
-
-
-cur.execute("DROP TABLE IF EXISTS Vsebuje CASCADE")
-cur.execute("""CREATE TABLE Vsebuje(Lokacija INT REFERENCES Lokacija(Id) ON UPDATE CASCADE ON DELETE CASCADE,
-Znacilnost INT REFERENCES Znacilnosti(Id) ON UPDATE CASCADE ON DELETE CASCADE, PRIMARY KEY (Lokacija, Znacilnost))""")
-cur.execute("INSERT INTO Vsebuje VALUES (1, 1)")
-cur.execute("INSERT INTO Vsebuje VALUES (1, 4)")
+#cur.execute("INSERT INTO Znacilnosti (Ime_znacilnosti) VALUES ('smučišče')")
+#cur.execute("INSERT INTO Znacilnosti (Ime_znacilnosti) VALUES ('plaža')")
+#cur.execute("INSERT INTO Znacilnosti (Ime_znacilnosti) VALUES ('center mesta')")
+#cur.execute("INSERT INTO Znacilnosti (Ime_znacilnosti) VALUES ('gore')")
 
 
 cur.execute("DROP TABLE IF EXISTS Uporabnik CASCADE")
